ed1bd0c173acc09e8f591b57fcad2bf57325d29b
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-07-14 14:19:29 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -25,7 +25,6 @@ urlpatterns = [
     path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('swagger.json/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+]
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
