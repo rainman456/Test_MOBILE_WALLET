72aa2f07c60964585fca8209c53293a1f44c3878
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-07-14 08:37:28 -0700

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -19,7 +19,7 @@ public=True
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.users.urls')),
+    path('', include('users.urls')),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
