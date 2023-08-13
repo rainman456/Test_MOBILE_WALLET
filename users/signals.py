168f7296292cb0f2b10a5e335ffbece447a6c610
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-08-13 08:05:34 -0700

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -2,8 +2,9 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from .models import UserProfile
+from userwallets.models import WalletStats
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=UserProfile)
 def create_wallet(sender, created, instance, *args, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        WalletStats.objects.create(owner=instance)
