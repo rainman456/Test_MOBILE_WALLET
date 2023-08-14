28393bd286f8a8dd11df57d7b2f375fac103695b
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-08-14 06:27:33 -0700

    Update views.py

--- userwallets/views.py
+++ userwallets/views.py
@@ -72,7 +72,7 @@ class GetVirtualAcnView(APIView):
                     owner.save()
                     return JsonResponse({'detail': 'Account number and id saved successfully'}, status=200)
                 else:
-                    return Response({'error':"unable to get account number"},status=status.HTTP_400_BAD_REQUEST)
+                    return Response({'error':"unable to get account number due to:",response.json()},status=status.HTTP_400_BAD_REQUEST)
             except UserProfile.DoesNotExist:
                 return JsonResponse({'detail': 'Invalid user '}, status=400)
 
