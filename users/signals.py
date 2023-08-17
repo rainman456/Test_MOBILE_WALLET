5edc1a7e02949af3c346cf313218e71254c8ecb5
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-08-17 11:12:06 -0700

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -3,12 +3,14 @@ from django.db.models.signals import post_save
 from django.dispatch.dispatcher import receiver
 from .models import UserProfile
 from userwallets.models import WalletStats
-"""
-@receiver(post_save, sender=User)
+from transactions.models import *
+
+
+@receiver(post_save, sender=UserProfile)
 def create_user(sender, created, instance, *args, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
-"""
+        Transactions.objects.create(account=instance)
+
 
 
 @receiver(post_save, sender=UserProfile)
