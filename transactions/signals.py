c6ee41b12519c485b03f33e3386033132762a3ea
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-08-17 15:54:02 -0700

    Update signals.py

--- transactions/signals.py
+++ transactions/signals.py
@@ -3,7 +3,7 @@ from django.dispatch.dispatcher import receiver
 from .models import Transactions,Deposits,Transfers,Mobile_TopUp
 
 @receiver(post_save, sender=Deposits)
-def create_wallet(sender, created, instance, *args, **kwargs):
+def create_deposit(sender, created, instance, *args, **kwargs):
     if created:
         deposit_type='wallet_deposit'
         Transactions.objects.create(
@@ -15,25 +15,27 @@ def create_wallet(sender, created, instance, *args, **kwargs):
 
 
 @receiver(post_save, sender=Transfers)
-def create_wallet(sender, created, instance, *args, **kwargs):
+def create_transfer(sender, created, instance, *args, **kwargs):
     if created:
         if instance.transfer_type=='bank_transfer':
-            Transactions.objects.create(
+           transaction=Transactions.objects.create(
                 transaction_type=instance.transfer_type,
                 account=instance.sender,
                 amount=instance.amount,
                 recipient=instance.recipient)
+            return transaction
         elif instance.transfer_type=='wallet_transfer':
-            Transactions.objects.create(
+           transaction=Transactions.objects.create(
                 transaction_type=instance.transfer_type,
                 account=instance.sender,
                 amount=instance.amount,
                 recipient=instance.receiver)
+            return transaction
 
 
 
 @receiver(post_save, sender=Mobile_TopUp)
-def create_wallet(sender, created, instance, *args, **kwargs):
+def create_mobile(sender, created, instance, *args, **kwargs):
     if created:
         transaction_type='mobile_purchase'
         Transactions.objects.create(
