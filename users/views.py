5b2d5579221ef6823207ac70cbf6e0ce144bdbe3
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-08-18 05:17:28 -0700

    Update views.py

--- users/views.py
+++ users/views.py
@@ -27,8 +27,9 @@ global otp_secret,otp,otp_code
 otp_secret= pyotp.random_base32()
 otp = pyotp.TOTP(otp_secret, digits=4)
 otp_code=otp.now()
-@method_decorator(csrf_exempt,name='dispatch')
+#@method_decorator(csrf_exempt,name='dispatch')
 class CustomUserViewSet(UserViewSet):
+    @swagger_auto_schema(request_body=CreateUser)
     serializer_class=CreateUser
     def create(self, request, *args, **kwargs):
         serializer = CreateUser(data=request.data)
