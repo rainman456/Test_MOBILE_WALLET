c579555097231774675a51c05efc35a0b1f33779
rainman456 <81997753+rainman456@users.noreply.github.com>
2023-10-31 03:54:04 -0700

    Delete userwallets directory

--- userwallets/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-@admin.register(WalletStats)
-class WalletssAdmin(admin.ModelAdmin):
-    list_display=('owner','wallet_id','is_disabled','balance','currency','virtual_acn','created_at',)
-    search_fields=['owner']
-   
-
